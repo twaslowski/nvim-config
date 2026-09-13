ebf8dbc3f31ac05fcfbced469a7950da29b2300c
Tobias Waslowski <tobias.waslowski@voize.de>
2026-09-13 16:47:12 +0200

    Install kustomize.nvim

---
+++ lua/plugins/kustomize.lua
@@ -0,0 +1,11 @@
+return {
+  "allaman/kustomize.nvim",
+  dependencies = {
+    "nvim-neo-tree/neo-tree.nvim",
+    "A7Lavinraj/fyler.nvim",
+    "L3MON4D3/LuaSnip",
+  },
+  ft = "yaml",
+  ---@type KustomizeConfig
+  opts = {},
+}
